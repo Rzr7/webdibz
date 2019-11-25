3ffd50d02a12343443b80e542249b3f865d9df32
Zackyy1 <qwertyuk01@gmail.com>
2019-11-25 22:57:27 +0200

    login button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from 'src/shared/database.service';
 import { Router } from '@angular/router';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'webdibz';
   
   constructor(
     private db: DatabaseService,
     private router: Router
     ) {
-      
+
   }
+
+  ngOnInit() {
+    console.log('This auth', this.db.afAuth.auth)
+  }
+ 
 
 }
--- src/shared/database.service.ts
+++ src/shared/database.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
 
 
 @Injectable({
@@ -11,11 +13,19 @@ export class DatabaseService {
 items: Observable<any[]>
 
   constructor(
-    private db: AngularFirestore
+    public db: AngularFirestore,
+    public afAuth: AngularFireAuth
     ) { 
       this.items = db.collection('templates').valueChanges();
       this.items.subscribe(e=>{
         console.log(e)
       })
    }
+
+   login() {
+     !this.afAuth.auth.currentUser ?
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()) :
+    this.afAuth.auth.signOut()
+  }
+
 }
